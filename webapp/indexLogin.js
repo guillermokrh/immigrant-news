e953c9c97145fe9b7195e32fd9d67870673200fe
Viren Mody <VirenMody@users.noreply.github.com>
2017-11-27 22:15:32 -0600

    Move Firebase initialization to js file

---
+++ webapp/indexLogin.js
@@ -0,0 +1,11 @@
+
+// Initialize Firebase
+var config = {
+    apiKey: "AIzaSyA-I9XVJya_5zR-BsXSYct7W8zCxkPh7PM",
+    authDomain: "immigrant-news.firebaseapp.com",
+    databaseURL: "https://immigrant-news.firebaseio.com",
+    projectId: "immigrant-news",
+    storageBucket: "immigrant-news.appspot.com",
+    messagingSenderId: "598567031572"
+};
+firebase.initializeApp(config);
