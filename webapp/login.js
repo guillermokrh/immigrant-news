d529bc871b642404e5174c007762376a4495fa4b
Viren Mody <VirenMody@users.noreply.github.com>
2017-12-01 17:49:08 -0600

    Moved modal script to login.js

--- webapp/login.js
+++ webapp/login.js
@@ -1,4 +1,13 @@
 (function() {
+
+    // Open and close the modal
+    const loginModal = document.getElementById('loginModal');
+    window.onclick = function(event) {
+        if (event.target === loginModal) {
+            loginModal.style.display = "none";
+        }
+    };
+
     // Initialize Firebase
     var config = {
         apiKey: "AIzaSyA-I9XVJya_5zR-BsXSYct7W8zCxkPh7PM",
